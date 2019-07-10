f81f9af99271536b684c3faab4fe9f0fb44e75db
Ali-D-Akbar <AliAkbarIT019@gmail.com>
2019-07-10 16:39:39 +0500

    Scrapy4

--- quotes_spider.py
+++ quotes_spider.py
@@ -1,23 +1,32 @@
 import scrapy
 
 
-class QuotesSpider(scrapy.Spider):
-    name = "quotes"
-    start_urls = [
-        'http://quotes.toscrape.com/page/1/',
-    ]
+class AuthorSpider(scrapy.Spider):
+    """
+    use this command to run
+    scrapy crawl author -o authors.json
+
+    extracts information about authors written in quotes from multiple pages
+    """
+    name = 'author'
+
+    start_urls = ['http://quotes.toscrape.com/']
 
     def parse(self, response):
-        """
-        Recursively calls the parse function until there is a next page. We don't need multiple addresses this way
-        """
-        for quote in response.css('div.quote'):
-            yield {
-                'text': quote.css('span.text::text').get(),
-                'author': quote.css('span small::text').get(),
-                'tags': quote.css('div.tags a.tag::text').getall(),
-            }
+        # follow links to author pages
+        for href in response.css('.author + a::attr(href)'):
+            yield response.follow(href, self.parse_author)
 
-        next_page = response.css('li.next a::attr(href)').get()
-        if next_page is not None:
-            yield response.follow(next_page, callback=self.parse)
+        # follow pagination links
+        for href in response.css('li.next a::attr(href)'):
+            yield response.follow(href, self.parse)
+
+    def parse_author(self, response):
+        def extract_with_css(query):
+            return response.css(query).get(default='').strip()
+
+        yield {
+            'name': extract_with_css('h3.author-title::text'),
+            'birthdate': extract_with_css('.author-born-date::text'),
+            'bio': extract_with_css('.author-description::text'),
+        }
